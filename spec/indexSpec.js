d478016773af24c5d1f3b0d5e09ba3ca1ad1e110
Mike Holm <mholm@rallydev.com>
2015-09-01 09:07:16 -0600

    `npm run watch` works with the test files

--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -1,7 +1,23 @@
 const i = require('../index.js');
 
-describe('foo', function() {
-  it('bar', function() {
-    expect(i()).toBe(1);
+describe('index.js', function() {
+  describe('when the input is not in the map', function() {
+    it('returns the user\'s input', function() {
+      expect(i()).toBe(undefined);
+    });
+
+    it('returns strings', function() {
+      expect(i('2')).toBe('2');
+    });
+
+    it('returns bigger numbers', function() {
+      expect(i(45)).toBe(45);
+    });
+  });
+
+  describe('when the input is in the map', function() {
+    it('returns the spelling', function() {
+      expect(i(2)).toBe('two');
+    });
   });
 });
